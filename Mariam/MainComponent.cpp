46e340be435bcf33f7a84f990e46beee07a118ee
Mariam <mariamazzam450@gmail.com>
2025-10-21 14:05:48 +0300

    loop

---
+++ Mariam/MainComponent.cpp
@@ -0,0 +1,74 @@
+﻿#pragma once
+#include "MainComponent.h"
+
+MainComponent::MainComponent() : player(), gui(player) {
+    addAndMakeVisible(gui);
+    setSize(500, 1080);
+    setAudioChannels(0, 2);
+}
+
+MainComponent::~MainComponent()
+{
+    shutdownAudio();
+}
+
+void MainComponent::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
+{
+    player.prepareToPlay(samplesPerBlockExpected, sampleRate);
+}
+
+void MainComponent::getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill)
+{
+    bufferToFill.clearActiveBufferRegion();
+    player.getNextAudioBlock(bufferToFill);
+}
+
+void MainComponent::releaseResources()
+{
+    player.releaseResources();
+}
+
+void MainComponent::start() {
+    player.start();
+}
+
+void MainComponent::stop() {
+    player.stop();
+}
+void MainComponent::setPosition(double pos) {
+    player.setPosition(pos);
+}
+void MainComponent::setGain(float gain) {
+    player.setGain(gain);
+}
+
+bool MainComponent::load(const juce::File& file) {
+    return player.load(file);
+}
+double MainComponent::getPosition() const {
+    return player.getPosition();
+}
+double MainComponent::getLength() const {
+    return player.getLength();
+}
+void MainComponent::mute() {
+    player.mute();
+}
+
+void MainComponent::play_pause() {
+    player.play_pause();
+}
+
+void MainComponent::paint(juce::Graphics& g)
+{
+    gui.paint(g);
+}
+
+void MainComponent::resized()
+{
+    gui.setBounds(0, 0, getWidth(), getHeight());
+}
+void MainComponent::loop() {
+    player.loop();
+}
+
---
+++ Mariam/MainComponent.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <JuceHeader.h>
+#include <vector>
+#include "PlayerGUI.h"
+#include "PlayerAudio.h"
+
+class MainComponent : public juce::AudioAppComponent {
+public:
+    MainComponent();
+    ~MainComponent();
+
+    // Audio
+    void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
+    void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override;
+    void releaseResources() override;
+
+    void start();
+    void stop();
+    void setPosition(double pos);
+    void setGain(float gain);
+    void play_pause();
+    bool load(const juce::File& file);
+    double getPosition() const;
+    double getLength() const;
+    void mute();
+    void loop();
+
+
+    // GUI
+    void paint(juce::Graphics& g);
+    void resized();
+
+private:
+    PlayerAudio player;
+    PlayerGUI gui;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent);
+};
